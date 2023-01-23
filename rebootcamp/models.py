94ee1660fc661b3e6407745cf6222ba5adbb62de
niksirbi <niko.sirbiladze@gmail.com>
2023-01-23 18:42:12 +0000

    Created BasicNN class with init and forward methods

--- rebootcamp/models.py
+++ rebootcamp/models.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-from typing import List, Tuple, Union
+from typing import List, Set, Union
 
-# import jax.numpy as jnp
+import jax.numpy as jnp
+
 # import matplotlib.pyplot as plt
 import numpy as np
-
-# from jax import grad, jit, nn
+from jax import nn
 
 # Specify path to data
 data_dir = Path(__name__).parent / "data"
@@ -19,51 +19,87 @@ x = np.expand_dims(x, 1)
 y = np.expand_dims(y, 1)
 print(x.shape, y.shape)
 
-# Initialize network with variable n_layers and n_neurons per layer
+
+class BasicNN:
+    """Basic neural network class."""
+
+    n_layers: int
+    n_neurons: Union[int, List[int]]
+    activation: str
+
+    def __init__(
+        self,
+        n_hidden_layers,
+        n_neurons,
+        activation=Set("sigmoid", "relu", "tanh"),
+    ):
+        """Initialize network with variable n_layers and n_neurons per layer.
+
+        Parameters
+        ----------
+        n_hidden_layers : int
+            Number of hidden layers
+        n_neurons : int or list of int
+            Number of neurons per layer.
+            If int, all layers have the same number of neurons.
+        activation : str, optional
+            Activation function.
+            Must be "sigmoid", "relu", or "tanh".
+            Defaults to "sigmoid"
+
+        """
+        self.n_hidden_layers = n_hidden_layers
+        self.n_neurons = n_neurons
+        self.activation = activation
+
+        # initialize weights and biases lists for each layer
+        self.W_list = []
+        self.B_list = []
+        for i in range(n_hidden_layers):
+            # initialize random weights and biases for each layer
+            weights = np.random.rand(n_neurons) - 0.5
+            biases = np.random.rand(n_neurons) - 0.5
+            # add extra dimension
+            weights = np.expand_dims(weights, 0)
+            biases = np.expand_dims(biases, 0)
+            # append to list
+            self.W_list.append(weights)
+            self.B_list.append(biases)
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """Forward pass of the network.
+
+        Parameters
+        ----------
+        x : np.ndarray
+            Input data.
+
+        Returns
+        -------
+        np.ndarray
+            Output of the network.
+
+        """
+        # apply activation function
+        if self.activation == "sigmoid":
+            activation = nn.sigmoid
+        elif self.activation == "relu":
+            activation = nn.relu
+        elif self.activation == "tanh":
+            activation = nn.tanh
+        else:
+            raise ValueError("activation must be sigmoid, relu, or tanh")
+
+        # forward pass
+        for i in range(self.n_hidden_layers):
+            interim = activation(jnp.dot(self.W_list[i], x) + self.B_list[i])
+            # Pooling for single neuron output
+            output = jnp.sum(interim)
+
+        return output
+
+
+# Initialize network
 n_layers = 2
 n_neurons = 10
-
-
-def initialize_network(
-    n_layers: int, n_neurons: Union[int, List[int]]
-) -> Tuple[List[np.ndarray], List[np.ndarray]]:
-    """Initialize network with variable n_layers and n_neurons per layer.
-
-    Parameters
-    ----------
-    n_layers : int
-        Number of layers.
-    n_neurons : int or list of int
-        Number of neurons per layer.
-        If int, all layers have the same number of neurons.
-
-    """
-
-    # validate inputs
-    if type(n_neurons) == int:
-        n_neurons_per_layer = [n_neurons] * n_layers
-    elif type(n_neurons) == list:
-        if len(n_neurons) != n_layers:
-            raise ValueError("n_neurons list must be of length n_layers")
-        else:
-            if not all(isinstance(x, int) for x in n_neurons):
-                raise TypeError("all elements of n_neurons list must be int")
-            n_neurons_per_layer = n_neurons
-    else:
-        raise TypeError("n_neurons must be int or list of int")
-
-    # initialize weights and biases lists for each layer
-    W_list = []
-    B_list = []
-    for i in range(n_layers):
-        # initialize random weights and biases for each layer
-        weights = np.random.rand(n_neurons_per_layer[i]) - 0.5
-        biases = np.random.rand(n_neurons_per_layer[i]) - 0.5
-        # add extra dimension
-        weights = np.expand_dims(weights, 0)
-        biases = np.expand_dims(biases, 0)
-        # append to list
-        W_list.append(weights)
-        B_list.append(biases)
-
-    return (W_list, B_list)
+net = BasicNN(n_layers, n_neurons, activation="sigmoid")
